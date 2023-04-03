83a29b7cc93b688a5eff700d72717e6cac4eaf86
gsimonov <jirisimonov@gmail.com>
2023-04-03 11:15:10 +0300

    feat: add elint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'asgk-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ASGK-GROUP';
+  public title = 'ASGK-GROUP';
 }
