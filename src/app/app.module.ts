e55f0354d5ae90566eb4a0f14755636d7d3e3c0c
gsimonov <jirisimonov@gmail.com>
2023-04-04 10:28:15 +0300

    feat: add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AsgkInterceptor } from './core/interceptors/asgk.interceptor';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule, CoreModule],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: AsgkInterceptor, multi: true }],
   bootstrap: [AppComponent],
 })
